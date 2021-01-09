d6a8edeca4ba113134dc1b8f982b7a641a14ac2e
aplana <igor.sidorov.190795@gmail.com>
2021-01-10 02:30:24 +0300

    Fix 3 Les

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,12 +1,13 @@
 public class Lesson3 {
     public static void main(String[] args) {
         int[] Array1 = new int[5];
-        int length = Array1.length;
         Array1[0] = 1;
         Array1[1] = 2;
         Array1[2] = 4;
         Array1[3] = 6;
         Array1[4] = 0;
-        System.out.println(Array1[0]+ Array1[2]);
+        int last = Array1[Array1.length / 2];
+        System.out.println(Array1[0]+ last);
+
     }
 }
