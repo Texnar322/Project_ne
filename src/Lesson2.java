708cedd3329b811a9f05bd0c0a9b4d5638f365ac
aplana <igor.sidorov.190795@gmail.com>
2021-01-10 02:09:18 +0300

    Add 3 Lesson

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,5 +1,6 @@
 public class Lesson2 {
     public static void main(String[] args) {
-
+        double a = 6.66;
+        byte b = 127;
     }
 }
--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,5 +1,12 @@
 public class Lesson3 {
     public static void main(String[] args) {
-
+        int[] Array1 = new int[5];
+        int length = Array1.length;
+        Array1[0] = 1;
+        Array1[1] = 2;
+        Array1[2] = 4;
+        Array1[3] = 6;
+        Array1[4] = 0;
+        System.out.println(Array1[0]+ Array1[2]);
     }
 }
